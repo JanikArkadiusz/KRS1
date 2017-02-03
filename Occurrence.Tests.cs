6acb9648421b07ea49c163e13af06d09710d0e67
JanikArkadiusz <janik.arkadiusz1@gmail.com>
2017-02-04 00:00:40 +0100

    Add files via upload

---
+++ Occurrence.Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit;
+using Repository.Models;
+
+namespace Repository.Tests
+{
+    public class OccurrenceTests
+    {
+        //TODO: 1. Czy podano poprawny typ, 
+        //TODO: 2. datę, 
+        //TODO: 3. nr zwierzęcia, 
+        //TODO: 4. czy zwierzę przebywa w stadzie itp...
+        //TODO: 5. brak numeru siedziby do/z 
+                  
+
+    }
+}
